18846e931bf9bbd042897ac100846ff9fa21279b
Ariel Chouminov <arielchoum@gmail.com>
2018-05-06 12:07:41 -0400

    Created initial setup and added 3d models

---
+++ Ramp-it/Ramp-it/Controller/RampPlacerVC.swift
@@ -0,0 +1,72 @@
+//
+//  ViewController.swift
+//  Ramp-it
+//
+//  Created by Ariel Chouminov on 2018-05-05.
+//  Copyright © 2018 Ariel Chouminov. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class RampPlacerVC: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        sceneView.delegate = self
+        sceneView.showsStatistics = true
+        let scene = SCNScene(named: "art.scnassets/pipe.dae")!
+        sceneView.scene = scene
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
